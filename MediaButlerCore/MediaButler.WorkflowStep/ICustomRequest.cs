5b4b6ed86c75114cd3409408acafe2cd6929d323
jpgarcia <jpgarcia@5011917-0405>
2016-10-14 15:51:24 -0400

    Add Custom Step

---
+++ MediaButlerCore/MediaButler.WorkflowStep/ICustomRequest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaButler.WorkflowStep
+{
+    /// <summary>
+    /// Workflow Custom request
+    /// </summary>
+    public interface ICustomRequest
+    {
+        /// <summary>
+        /// Media Services Asset ID
+        /// </summary>
+        string AssetId { get; set; }
+        /// <summary>
+        /// Media Services Account Name
+        /// </summary>
+        string MediaAccountName { get; set; }
+        /// <summary>
+        /// Media Services Account Key
+        /// </summary>
+        string MediaAccountKey { get; set; }
+        /// <summary>
+        /// Media services Storage connection string
+        /// </summary>
+        string MediaStorageConn { get; set; }
+        /// <summary>
+        /// Step configuration data
+        /// </summary>
+        string ConfigData { get; set; }
+        /// <summary>
+        /// Process start time
+        /// </summary>
+        DateTime TimeStampProcessingStarted { get; set; }
+        /// <summary>
+        /// Process LOG
+        /// </summary>
+        List<string> Log { get; set; }
+        /// <summary>
+        /// Process meta data
+        /// </summary>
+        Dictionary<string, string> MetaData { get; set; }
+        /// <summary>
+        /// Process tyoe ID
+        /// </summary>
+        string ProcessTypeId { get; set; }
+        /// <summary>
+        /// Instance process ID
+        /// </summary>
+        string ProcessInstanceId { get; set; }
+        /// <summary>
+        /// Process exceptions list
+        /// </summary>
+        List<string> Exceptions { get; set; }
+        /// <summary>
+        /// MBF process configuration storage connection string
+        /// </summary>
+        string ProcessConfigConn { get; set; }
+        /// <summary>
+        /// MBF process instance control file URL
+        /// </summary>
+        string ButlerRequest_ControlFileUri { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        List<string> ButlerRequest_MezzanineFiles { get; set; }
+        /// <summary>
+        /// Step configuraton data
+        /// </summary>
+        string StepConfiguration { get; set; }
+    }
+}
---
+++ MediaButlerCore/MediaButler.WorkflowStep/ICustomStepExecution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaButler.WorkflowStep
+{
+    /// <summary>
+    /// Custom workflow step
+    /// </summary>
+    public interface ICustomStepExecution
+    {
+        /// <summary>
+        /// Implement Custome logic Step
+        /// </summary>
+        /// <param name="request">Custom request</param>
+        /// <returns>true</returns>
+        bool execute(ICustomRequest request);
+    }
+}
